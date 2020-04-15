d5026d72958b510ac4bd6c687c84dc3dfa27867f
iriyanto <iriyanto@student.telkomuniversity.ac.id>
2020-04-15 17:12:26 +0700

    Update tugas MPI

---
+++ Tugas-5 MPI/01.mpi_p2p_terbesar.py
@@ -0,0 +1,21 @@
+# import mpi4py
+from mpi4py import MPI
+
+# buat COMM
+comm = MPI.COMM_WORLD
+
+# dapatkan rank proses
+rank = comm.Get_rank()
+
+# dapatkan total proses berjalan
+size = comm.Get_size()
+
+# jika saya rank terbesar maka saya akan mengirimkan pesan ke proses yang mempunyai rank 0 s.d rank terbesar-1
+if rank == size-1 :
+    for i in range(size-1):
+        comm.send("Hello rank {}".format(i), dest=i)
+
+# jika saya bukan rank terbesar maka saya akan menerima pesan yang berasal dari proses dengan rank terbesar
+else:
+    data = comm.recv(source=size-1)
+    print('rank ',rank,' menerima ',data)
